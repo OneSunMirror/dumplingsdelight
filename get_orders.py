ec5a36e7ee5a94667d399c56c0117bc4b7c34975
siwei.wu <u6735397@anu.edu.au>
2023-08-05 22:44:50 +1000

    distance matrix working

--- get_orders.py
+++ get_orders.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from optimize_route import find_route
 from requests.auth import HTTPBasicAuth
 #from get_secret import get_dumpling_secret
 def retreive_orders(secrets):
@@ -28,6 +29,7 @@ def retreive_orders(secrets):
     #    f.write(c)
     return response
 map_route = "www.google.com/maps/dir"
+locations = []
 f = open("orders_list.txt", "x", encoding="utf-8")
 #secrets = get_dumpling_secret()
 secrets = {"0e38bd38-e49b-487d-bbf1-f2a6a1e10549":"IST.eyJraWQiOiJQb3pIX2FDMiIsImFsZyI6IlJTMjU2In0.eyJkYXRhIjoie1wiaWRcIjpcIjNjZmI0MGFkLTIyNGItNGM2Yy04OGVjLTg3Y2QxNTFmNWRmMVwiLFwiaWRlbnRpdHlcIjp7XCJ0eXBlXCI6XCJhcHBsaWNhdGlvblwiLFwiaWRcIjpcIjZmOTlkMjdjLWU2NWQtNDMyMi1iNjY0LWIzMzE4ZWViYmJlOFwifSxcInRlbmFudFwiOntcInR5cGVcIjpcImFjY291bnRcIixcImlkXCI6XCIwZTM4YmQzOC1lNDliLTQ4N2QtYmJmMS1mMmE2YTFlMTA1NDlcIn19IiwiaWF0IjoxNjkwOTc1NTAzfQ.k56pNNwuVHQXKfgMXoh2PJGraKjJwglvIi8XOMQu4gt3Ri3XYQ2w04V5YyUh-Cxwax5WpIvn7goLqyMWIvNcUejevn-TgB0H0mu7y6bCnTYaJWdtKdZ7oc71zR5gEhdz-KC3Ok-p0VT7g2Jc2TbCrVMEFUR55qZGCK1LCkFg9g2gXYfl-c-z4yl818BbhpZts8h0CtjPYht5fZi3wIUsdDxbgRPjNfKvRHMIu5MyhX6_JtcSVzp-25nJSM6sPrOKA8P1xku_KeVUyjkc-VPuyH61eGDiXGNjW9AXLpU232ncS9MbNk2CzVO1vgxhzCZX8kbwXhRCVku_xLp-wjQKbA"}
@@ -49,6 +51,7 @@ for order in orders['orders']:
     if 'addressLine2' in add_dict:
         add = add + " " + add_dict['addressLine2']
     add = add + ", " + add_dict['city'] + ", " + add_dict['zipCode']
+    locations.append(add)
     map_route = map_route + "/" + add
     f.write("Address: " + add)
     f.write("\n")
@@ -63,3 +66,4 @@ f.write("\n\n\n")
 f.write("Google Map Link:\n")
 f.write(map_route)
 f.close()
+print(find_route(locations))
--- optimize_route.py
+++ optimize_route.py
@@ -1,4 +1,9 @@
 import requests
 import googlemaps
+from googlemaps import distance_matrix
 url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="
 map_api_key = "AIzaSyDILOATa9jdhX1tQqsYiS-ALgKSzZ6pesM"
+gmaps = googlemaps.Client(key=map_api_key)
+def find_route(locations):
+    gmaps = googlemaps.Client(key=map_api_key)
+    return gmaps.distance_matrix(locations, locations)
